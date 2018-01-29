73b5121e40ccc328a1d228d97c6c3af6458278bd
harinder.chagger <harinder.chagger@dal.ca>
2018-01-29 15:50:30 -0400

    sample program

---
+++ practicetask1.py
@@ -0,0 +1,7 @@
+some_var = 5
+if some_var > 10:
+    print("some_var is totally bigger than 10.")
+elif some_var < 10:    # This elif clause is optional.
+    print("some_var is smaller than 10.")
+else:                  # This is optional too.
+    print("some_var is indeed 10.")
